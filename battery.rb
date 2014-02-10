ea9f0650d44fe65148817f05c7dd0c336d3a03d3
chris-hallquist <challquist@gmail.com>
2014-02-09 16:47:02 -0800

    crit table, checking if spinal

--- battery.rb
+++ battery.rb
@@ -1,10 +1,34 @@
 class Battery
-  def initialize(ship)
+  attr_accessor :ship, :factor
+  
+  CRITICAL_HIT_TABLE = {
+    2 => "Ship Vaporized",
+    3 => "Bridge Destroyed",
+    4 => "Computer Destroyed",
+    5 => "Maneuver Drive Destroyed",
+    6 => "One Screen Disabled",
+    7 => "Jump Drive Disabled",
+    8 => "Hangar/Boat Deck Destoyed",
+    9 => "Power Plant Disabled",
+    10 => "Crew-1",
+    11 => "Spinal Mount/Fire Control Out",
+    12 => "Frozen Watch/Ship's Troops Dead"
+  }
+  
+  def initialize(ship, factor)
     @ship = ship
-    @ship.ship << this
+    @ship.batteries << this
+    @factor = factor
+  end
+  
+  def defenses_penetrated?(target)
+    raise "Not implemented"
   end
   
   def fire(target)
+    if roll > to_hit(target) && defenses_penetrated?
+      # proceed to the damage tables
+    end
   end
 
   def roll(n=2)
@@ -47,14 +71,8 @@ class MesonGun < Battery
 end
 
 class ParticleAccelerator < Battery
-  attr_reader :factor
-  
-  def initialize(factor)
-    @factor = factor
-  end
-  
   def attack_table
-    if factor.class == Fixnum
+    if factor.is_a? Fixnum
       if factor < 3
         return 10 - factor
       else
@@ -71,6 +89,19 @@ class ParticleAccelerator < Battery
         return 0
       end
     end
+  end
+  
+  def defenses_penetrated?(target)
+    true
+    # No defenses are possible against a particle accelerator
+  end
+  
+  def roll_damage
+  
+  end
+  
+  def spinal?
+    factor.is_a? String
   end
   
   def to_hit(target)
