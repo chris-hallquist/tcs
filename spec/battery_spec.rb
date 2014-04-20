8b810b7c95336819ba950e96e16d02c8280bde06
chris-hallquist <challquist@gmail.com>
2014-04-20 13:49:20 -0700

    Wrote more tests for battery class

--- spec/battery_spec.rb
+++ spec/battery_spec.rb
@@ -3,11 +3,20 @@ require 'ship'
 
 describe Battery do
   let(:e) { Eurisko.new }
+  let(:w) { Wasp.new }
   let(:m) { Missile.new(3, 1, 6) }
   let(:g) { MesonGun.new(12, 1, 6) }
   context 'when the battery is a Missile' do
     it 'should always hit Eurisko-class ships' do
-      expect((0...72).all? { m.hit?(e, :long) }).to be_true
+      srand 0
+      expect((0..71).all? { m.hit?(e, :long) }).to be_true
+    end
+    it 'should usually hit Wasp-class ships' do
+      srand 0
+      expect((0..7).all? { m.hit?(w, :long) }).to be_true
+    end
+    it 'should sometimes miss Wasp-class ships' do
+      expect(m.hit?(w, :long)).to be_false
     end
   end
 end
